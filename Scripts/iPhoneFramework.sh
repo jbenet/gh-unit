c94ca3364ecf34f0c62ba1ceda231203bb104c27
jbenet <jbenet@cs.stanford.edu>
2010-05-20 21:26:51 -0700

    Fixed framework

--- Scripts/iPhoneFramework.sh
+++ Scripts/iPhoneFramework.sh
@@ -5,30 +5,30 @@
 
 # Should define FLAVOR=2_1,2_1CL,3_0,3_0CL
 # Define these to suit your nefarious purposes
-                 FRAMEWORK_NAME=GHUnitIPhone${FLAVOR}
+                 FRAMEWORK_NAME=GHUnitIPhone
                        LIB_NAME=libGHUnitIPhone
               FRAMEWORK_VERSION=A
       FRAMEWORK_CURRENT_VERSION=${FLAVOR}
 FRAMEWORK_COMPATIBILITY_VERSION=${FLAVOR}
-                     BUILD_TYPE=Release
 
-# Where we'll put the build framework.
-# The script presumes we're in the project root
-# directory. Xcode builds in "build" by default
-FRAMEWORK_BUILD_PATH="build/Framework"
+# Tests file to include in projects
+TESTS_MAIN_FILE=GHUnitIPhoneTestMain.m
 
-# Clean any existing framework that might be there
-# already
+
+# Paths
+COMBINED_LIB=${BUILD_DIR}/Combined${BUILD_STYLE}${FLAVOR}/${LIB_NAME}${FLAVOR}.a
+ZIP_DIR=${BUILD_DIR}/Zip
+FRAMEWORK_BUILD_PATH="${BUILD_DIR}/${BUILD_STYLE}-Framework${FLAVOR}"
+
+# This is the full name of the framework we'll build
+FRAMEWORK_DIR=$FRAMEWORK_BUILD_PATH/$FRAMEWORK_NAME.framework
+
+# Clean any existing framework that might be there already
 echo "Framework: Cleaning framework..."
 [ -d "$FRAMEWORK_BUILD_PATH" ] && \
   rm -rf "$FRAMEWORK_BUILD_PATH"
 
-# This is the full name of the framework we'll
-# build
-FRAMEWORK_DIR=$FRAMEWORK_BUILD_PATH/$FRAMEWORK_NAME.framework
-
-# Build the canonical Framework bundle directory
-# structure
+# Build the canonical Framework bundle directory structure
 echo "Framework: Setting up directories..."
 mkdir -p $FRAMEWORK_DIR
 mkdir -p $FRAMEWORK_DIR/Versions
@@ -42,28 +42,33 @@ ln -s Versions/Current/Headers $FRAMEWORK_DIR/Headers
 ln -s Versions/Current/Resources $FRAMEWORK_DIR/Resources
 ln -s Versions/Current/$FRAMEWORK_NAME $FRAMEWORK_DIR/$FRAMEWORK_NAME
 
-# Check that this is what your static libraries
-# are called
-ARM_FILES="build/$BUILD_TYPE-iphoneos/${LIB_NAME}Device${FLAVOR}.a"
-I386_FILES="build/$BUILD_TYPE-iphonesimulator/${LIB_NAME}Simulator${FLAVOR}.a"
+cp $COMBINED_LIB $FRAMEWORK_DIR/Versions/Current/$FRAMEWORK_NAME
 
-# The trick for creating a fully usable library is
-# to use lipo to glue the different library
-# versions together into one file. When an
-# application is linked to this library, the
-# linker will extract the appropriate platform
-# version and use that.
-# The library file is given the same name as the
-# framework with no .a extension.
-echo "Framework: Creating library..."
-lipo \
-  -create \
-  -arch armv6 "$ARM_FILES" \
-  -arch i386 "$I386_FILES" \
-  -o "$FRAMEWORK_DIR/Versions/Current/$FRAMEWORK_NAME"
+# Unneeded, as CombineLibs.sh makes these
+#
+# # Check that this is what your static libraries
+# # are called
+# ARM_FILES="${BUILD_DIR}/${BUILD_TYPE}-iphoneos/${LIB_NAME}Device${FLAVOR}.a"
+# I386_FILES="${BUILD_DIR}/${BUILD_TYPE}-iphonesimulator/${LIB_NAME}Simulator${FLAVOR}.a"
+#
+# # The trick for creating a fully usable library is
+# # to use lipo to glue the different library
+# # versions together into one file. When an
+# # application is linked to this library, the
+# # linker will extract the appropriate platform
+# # version and use that.
+# # The library file is given the same name as the
+# # framework with no .a extension.
+# echo "Framework: Creating library..."
+# lipo \
+#   -create \
+#   -arch armv6 "$ARM_FILES" \
+#   -arch i386 "$I386_FILES" \
+#   -o "$FRAMEWORK_DIR/Versions/Current/$FRAMEWORK_NAME"
 
-# Now copy the final assets over: your library
-# header files and the plist file
-echo "Framework: Copying assets into current version..."
+# Now copy the header files and the plist file
+echo "Framework: Copying headers and plist into current version..."
+cp ../Classes/*.h $FRAMEWORK_DIR/Headers/
 cp ../Classes-IPhone/*.h $FRAMEWORK_DIR/Headers/
 cp Framework.plist $FRAMEWORK_DIR/Resources/Info.plist
+cp ../Classes-IPhone/GHUnitIPhoneTestMain.m $FRAMEWORK_DIR/Resources/.
